cb1451622e0316393a9c0538fd7bb2f99fc62175
danny9996 <59576826+danny9996@users.noreply.github.com>
2021-08-18 20:23:47 -0400

    todo tasks

--- BreadmakerReport/Program.cs
+++ BreadmakerReport/Program.cs
@@ -3,6 +3,8 @@ using System;
 using System.Linq;
 using RatingAdjustment.Services;
 using BreadmakerReport.Models;
+using System.Collections.Generic;
+
 
 namespace BreadmakerReport
 {
@@ -16,8 +18,14 @@ namespace BreadmakerReport
             Console.WriteLine("Welcome to Bread World");
             var BreadmakerDb = new BreadMakerSqliteContext(dbfile);
             var BMList = BreadmakerDb.Breadmakers
-                // TODO: add LINQ logic ...
-                //       ...
+                .Include(i => i.Reviews)
+                .AsEnumerable()
+                .Select(re => new {
+                    Reviews = re.Reviews.Count,
+                    Average = Math.Round(re.Reviews.Average(s => s.stars), 2),
+                    Adjust = Math.Round(ratingAdjustmentService.Adjust(re.Reviews.Average(s => s.stars), re.Reviews.Count()), 2),
+                    re.title
+                }).OrderByDescending(a => a.Adjust)
                 .ToList();
 
             Console.WriteLine("[#]  Reviews Average  Adjust    Description");
@@ -25,7 +33,7 @@ namespace BreadmakerReport
             {
                 var i = BMList[j];
                 // TODO: add output
-                // Console.WriteLine( ... );
+                Console.WriteLine("[{0}] {1} {2} {3} {4}", j + 1, i.Reviews, i.Average, i.Adjust, i.title);
             }
         }
     }
